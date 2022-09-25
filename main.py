7d5839f710fd167ca89526818139a71196d93e79
Yani-Jivkov <114335628+Yani-Jivkov@users.noreply.github.com>
2022-09-25 02:49:52 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Първи скрипт на питон
+# Конвертор на валута лев към евро    REV.1.0
+# Яни Живков
+# yani.jivkov@gmail.com
+# 25.09.2022
+
+leva = int(input())
+euro = leva / 1.95583
+print(euro)
